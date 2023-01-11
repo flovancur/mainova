9df1759e850d173a7b437204afb49f0562949b97
Florian Vancura <florian_vancura@gmx.de>
2023-01-11 13:25:34 +0100

    Anpassen des Datenformats der GET-Anfragen

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,27 @@ import mongoose from "mongoose";
 import express from 'express';
 // import * as swaggerUi from 'swagger-ui-express';
 //import * as swaggerDocument from './docs/swagger.json';
-
-
-
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 3000;
 let hostname = process.env.HOSTNAME || 'localhost';
-const url = process.env.DATABASE || '127.0.0.1:27017';
-
-
-
-
+const url = process.env.DATABASE || 'mongodb://127.0.0.1:27017/mainova';
 //Verbindung zu MongoDB aufbauen.
 const mainovaDb = async () => {
-    await mongoose.connect(`mongodb://${url}/mainova`).then(() => console.log('Connected to Database'));
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(url).then(() => console.log('Connected to Database'));
 }
 mainovaDb().catch((err) => console.log(err));
-
-
 websocketParking(); // Startet die Parking-Websocketverbindung
 websocketEnvironment(); // Startet die Environment-Websocketverbindung
-
-
 app.use(express.json());
-
 app.use('/parking', parkingRoutes);
 app.use('/environment', environmentRoutes);
-
 /*app.use('/docs/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 const server = app.listen(port, () => {
     console.log('Server gestartet: http://' + hostname + ':' + port);
 });*/
-
-
 app.listen(port, ()=> console.log(`Server gestartet: http://${hostname}:${port}`));
-
 const closeConnection = () => {
     if (mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2) {
         mongoose.connection.close().then(() => {
@@ -53,12 +37,9 @@ const closeConnection = () => {
     // server.close();
     process.exit();
 };
-
 process.on('SIGINT', function () {
     closeConnection();
 });
 process.on('SIGTERM', function () {
     closeConnection();
 });
-
-
